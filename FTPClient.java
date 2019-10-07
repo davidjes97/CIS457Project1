4956038b676c971cbcf52c0e26519da67eddfd16
davidjes97 <46247412+davidjes97@users.noreply.github.com>
2019-10-06 22:04:18 -0400

    Adding in client functionality
    Added in the else if statements for all of the commands and started to implement the quite command

--- FTPClient.java
+++ FTPClient.java
@@ -1,67 +1,120 @@
-import java.io.*; 
+import java.io.*;
 import java.net.*;
 import java.util.*;
 import java.text.*;
 import java.lang.*;
 import javax.swing.*;
-class FTPClient { 
 
-    public static void main(String argv[]) throws Exception 
-    { 
-        String sentence; 
-        String modifiedSentence; 
+class FTPClient {
+
+    public static void main(String argv[]) throws Exception {
+        String sentence;
+        String modifiedSentence;
         boolean isOpen = true;
-        int number=1;
+        int number = 1;
         boolean notEnd = true;
-	String statusCode;
-	boolean clientgo = true;
-	    
-	
-	BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in)); 
+        String statusCode;
+        boolean clientgo = true;
+
+
+        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
         sentence = inFromUser.readLine();
         StringTokenizer tokens = new StringTokenizer(sentence);
 
 
-	if(sentence.startsWith("connect")){
-	String serverName = tokens.nextToken(); // pass the connect command
-	serverName = tokens.nextToken();
-	port1 = Integer.parseInt(tokens.nextToken());
-        System.out.println("You are connected to " + serverName);
-        
-	Socket ControlSocket= new Socket(serverName, port1);
-        
-	while(isOpen && clientgo)
-        {      
-	      
-          DataOutputStream outToServer = new DataOutputStream(ControlSocket.getOutputStream()); 
-          
-	  DataInputStream inFromServer = new DataInputStream(new BufferedInputStream (ControlSocket.getInputStream()));
-          
-    	  sentence = inFromUser.readLine();
-	   
-        if(sentence.equals("list:"))
-        {
-            
-	    port = port +2;
-	    outToServer.writeBytes (port + " " + sentence + " " + '\n');
-	    
-            ServerSocket welcomeData = new ServerSocket(port);
-	    Socket dataSocket =welcomeData.accept(); 
+        if (sentence.startsWith("connect")) {
+            String serverName = tokens.nextToken(); // pass the connect command
+            serverName = tokens.nextToken();
+            port1 = Integer.parseInt(tokens.nextToken());
+            System.out.println("You are connected to " + serverName);
 
- 	    DataInputStream inData = new DataInputStream(new BufferedInputStream (dataSocket.getInputStream()));
-            while(notEnd) 
-            {
-                modifiedSentence = inData.readUTF();
+            try{
+                Socket ControlSocket = new Socket(serverName, port1);
+                clientgo = true;
+            }catch (exception e){
+                clientgo = false;
+                System.out.println(e);
+            }
+
+
+            while (isOpen && clientgo) {
+
+                DataOutputStream outToServer = new DataOutputStream(ControlSocket.getOutputStream());
+
+                DataInputStream inFromServer = new DataInputStream(new BufferedInputStream(ControlSocket.getInputStream()));
+
+                sentence = inFromUser.readLine();
+
+                if (sentence.equals("list:")) {
+
+                    port = port + 2;
+                    outToServer.writeBytes(port + " " + sentence + " " + '\n');
+
+                    ServerSocket welcomeData = new ServerSocket(port);
+                    Socket dataSocket = welcomeData.accept();
+
+                    DataInputStream inData = new DataInputStream(new BufferedInputStream(dataSocket.getInputStream()));
+                    while (notEnd) {
+                        modifiedSentence = inData.readUTF();
                ........................................
 	       ........................................
+                    }
+
+
+                    welcomeData.close();
+                    dataSocket.close();
+                    System.out.println("\nWhat would you like to do next: \n retr: file.txt ||stor: file.txt  || close");
+
+                } else if (sentence.startsWith("retr: ")) {
+
+                    port = port + 2;
+                    outToServer.writeBytes(port + " " + sentence + " " + '\n');
+
+                    ServerSocket welecomeData = new ServerSocket(port);
+                    Socket dataSocket = welecomeData.accept();
+
+                    DataInputStream inData = new DataInputStream(new BufferedInputStream(dataSocket.getInputStream()));
+                    while (notEnd) {
+                        modifiedSentence = inData.readUTF();
+               ........................................
+	       ........................................
+                    }
+
+
+                    welcomeData.close();
+                    dataSocket.close();
+                    System.out.println("\nWhat would you like to do next: \n retr: file.txt ||stor: file.txt  || close");
+                }
+
+                 else if (sentence.startsWith("stor:")){
+                    port = port + 2;
+                    outToServer.writeBytes(port + " " + sentence + " " + '\n');
+
+                    ServerSocket welecomeData = new ServerSocket(port);
+                    Socket dataSocket = welecomeData.accept();
+
+                    DataInputStream inData = new DataInputStream(new BufferedInputStream(dataSocket.getInputStream()));
+                    while (notEnd) {
+                        modifiedSentence = inData.readUTF();
+               ........................................
+	       ........................................
+                    }
+
+
+                    welcomeData.close();
+                    dataSocket.close();
+                    System.out.println("\nWhat would you like to do next: \n retr: file.txt ||stor: file.txt  || close");
+                }
+
+                } else if (sentence.equals("quit:")){
+
+                    port = port + 2;
+                    outToServer.writeBytes(sentence);
+
+                    ControlSocket.close();
+                    isOpen = false
+
+                } else {
+                    System.out.println("Error, incorrect command");
+                }
             }
-	
-
-	 welcomeData.close();
-	 dataSocket.close();
-	 System.out.println("\nWhat would you like to do next: \n retr: file.txt ||stor: file.txt  || close");
-
-        }
-         else if(sentence.startsWith("retr: "))
-        {
-		....................................................
