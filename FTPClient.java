8ac5816b2c8b70af87d46a4f506aa61098f6efb7
Isaac Dessert <desserti@mail.gvsu.edu>
2019-10-14 16:24:50 -0400

    commented dots and fixed brackets

--- FTPClient.java
+++ FTPClient.java
@@ -5,8 +5,8 @@ import java.text.*;
 import java.lang.*;
 import javax.swing.*;
 
-class FTPClient { 
-    //  private static int port = 4203;
+class FTPClient {
+    // private static int port = 4203;
     // private static InetAddress = host;
 
     public static void main(String argv[]) throws Exception {
@@ -19,40 +19,41 @@ class FTPClient {
         String statusCode;
         boolean clientgo = true;
 
-
         BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
         sentence = inFromUser.readLine();
         StringTokenizer tokens = new StringTokenizer(sentence);
 
-        //When the command connect is entered it will attempt a connection to the server and then allow for more commands
+        // When the command connect is entered it will attempt a connection to the
+        // server and then allow for more commands
         if (sentence.startsWith("connect")) {
             String serverName = tokens.nextToken(); // pass the connect command
             serverName = tokens.nextToken();
             port1 = Integer.parseInt(tokens.nextToken());
             System.out.println("You are connected to " + serverName);
 
-            //Attempts to connect to the server
+            // Attempts to connect to the server
             try {
                 Socket ControlSocket = new Socket(serverName, port1);
                 clientgo = true;
-            } catch (exception e) {//If the server does not exist an error is returned
+            } catch (exception e) {// If the server does not exist an error is returned
                 clientgo = false;
                 System.out.println(e);
             }
 
-            //Continous loop that stops only when a user requests to quit
+            // Continous loop that stops only when a user requests to quit
             while (isOpen && clientgo) {
 
                 DataOutputStream outToServer = new DataOutputStream(ControlSocket.getOutputStream());
 
-                DataInputStream inFromServer = new DataInputStream(new BufferedInputStream(ControlSocket.getInputStream()));
+                DataInputStream inFromServer = new DataInputStream(
+                        new BufferedInputStream(ControlSocket.getInputStream()));
 
                 sentence = inFromUser.readLine();
 
                 /*
-                When this command is sent to the server, the server returns a list of the
-                files in the current directory on which it is executing. The client should get the
-                list and display it on the screen.
+                 * When this command is sent to the server, the server returns a list of the
+                 * files in the current directory on which it is executing. The client should
+                 * get the list and display it on the screen.
                  */
                 if (sentence.equals("list:")) {
 
@@ -65,19 +66,19 @@ class FTPClient {
                     DataInputStream inData = new DataInputStream(new BufferedInputStream(dataSocket.getInputStream()));
                     while (notEnd) {
                         modifiedSentence = inData.readUTF();
-               ........................................
-	       ........................................
+                        // ........................................
+                        // ........................................
                     }
-
 
                     welcomeData.close();
                     dataSocket.close();
-                    System.out.println("\nWhat would you like to do next: \n retr: file.txt ||stor: file.txt  || close");
+                    System.out
+                            .println("\nWhat would you like to do next: \n retr: file.txt ||stor: file.txt  || close");
 
                 }
                 /*
-                RETR <filename>: This command allows a client to get a file specified by its
-                filename from the server.
+                 * RETR <filename>: This command allows a client to get a file specified by its
+                 * filename from the server.
                  */
                 else if (sentence.startsWith("retr: ")) {
 
@@ -90,18 +91,18 @@ class FTPClient {
                     DataInputStream inData = new DataInputStream(new BufferedInputStream(dataSocket.getInputStream()));
                     while (notEnd) {
                         modifiedSentence = inData.readUTF();
-               ........................................
-	       ........................................
+                        // ........................................
+                        // ........................................
                     }
-
 
                     welcomeData.close();
                     dataSocket.close();
-                    System.out.println("\nWhat would you like to do next: \n retr: file.txt ||stor: file.txt  || close");
+                    System.out
+                            .println("\nWhat would you like to do next: \n retr: file.txt ||stor: file.txt  || close");
                 }
                 /*
-                STOR <filename>: This command allows a client to send a file specified by its
-                filename to the server.
+                 * STOR <filename>: This command allows a client to send a file specified by its
+                 * filename to the server.
                  */
                 else if (sentence.startsWith("stor: ")) {
                     port = port + 2;
@@ -113,30 +114,33 @@ class FTPClient {
                     DataInputStream inData = new DataInputStream(new BufferedInputStream(dataSocket.getInputStream()));
                     while (notEnd) {
                         modifiedSentence = inData.readUTF();
-               ........................................
-	       ........................................
+                        // ........................................
+                        // ........................................
                     }
-
 
                     welcomeData.close();
                     dataSocket.close();
-                    System.out.println("\nWhat would you like to do next: \n retr: file.txt ||stor: file.txt  || close");
+                    System.out
+                            .println("\nWhat would you like to do next: \n retr: file.txt ||stor: file.txt  || close");
                 }
-                 /*
-                 QUIT: This command allows a client to terminate the control connection. On
-                 receiving this command, the client should send it to the server and terminate
-                 the connection. When the ftp_server receives the quit command it should
-                 close its end of the connection.
-                  */
+                /*
+                 * QUIT: This command allows a client to terminate the control connection. On
+                 * receiving this command, the client should send it to the server and terminate
+                 * the connection. When the ftp_server receives the quit command it should close
+                 * its end of the connection.
+                 */
                 else if (sentence.equals("quit:")) {
 
                     port = port + 2;
                     outToServer.writeBytes(sentence);
 
                     ControlSocket.close();
-                    isOpen = false
+                    isOpen = false;
 
                 } else {
                     System.out.println("Error, incorrect command");
                 }
             }
+        }
+    }
+}
