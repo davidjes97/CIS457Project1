4fbac674127b2d6db2c49cad373e87d94864992b
davidjes97 <davidjes@mail.gvsu.edu>
2019-10-14 22:41:29 -0400

    Finished list

--- FTPClient.java
+++ FTPClient.java
@@ -1,97 +1,93 @@
-import java.io.*; 
-import java.net.*;
-import java.util.*;
-import java.text.*;
-import java.lang.*;
-import javax.swing.*;
-class FTPClient { 
+import java.io. *;
+import java.net. *;
+import java.util. *;
+import java.text. *;
+import java.lang. *;
+import javax.swing. *;
+class FTPClient {
 
-    public static void main(String argv[]) throws Exception { 
-        String sentence; 
-        String modifiedSentence; 
+    public static void main(String argv[])throws Exception {
+        String sentence;
+        String modifiedSentence;
         boolean isOpen = true;
-        int number=1;
+        int number = 1;
         int port = 2328;
         boolean notEnd = true;
-	    String statusCode;
-	    boolean clientgo = true;
-	    
-	
-	    BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in)); 
+        String statusCode;
+        boolean clientgo = true;
+
+
+        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System. in));
         sentence = inFromUser.readLine();
         StringTokenizer tokens = new StringTokenizer(sentence);
 
 
-	    if(sentence.startsWith("connect")){
-	        String serverName = tokens.nextToken(); // pass the connect command
-	        serverName = tokens.nextToken();
-	        int port1 = Integer.parseInt(tokens.nextToken());
+        if (sentence.startsWith("connect")) {
+            String serverName = tokens.nextToken(); // pass the connect command
+            serverName = tokens.nextToken();
+            int port1 = Integer.parseInt(tokens.nextToken());
             System.out.println("You are connected to " + serverName);
-        
-            Socket ControlSocket= new Socket(serverName, port1);
-            
 
-	        while(isOpen && clientgo)
-            {      
+            Socket ControlSocket = new Socket(serverName, port1);
+
+            while (isOpen && clientgo) {
                 System.out.println("\nWhat would you like to do next: \n list: || retr: file.txt || stor: file.txt || quit ");
-                DataOutputStream outToServer = new DataOutputStream(ControlSocket.getOutputStream()); 
-	            DataInputStream inFromServer = new DataInputStream(new BufferedInputStream (ControlSocket.getInputStream()));
-          
-    	        sentence = inFromUser.readLine();
-	   
-                if(sentence.equals("list:"))
-                {
-            
-	                port = port + 2;
-	                outToServer.writeBytes (port + " " + sentence + " " + '\n');
-	    
-                    ServerSocket welcomeData = new ServerSocket(port);
-	                Socket dataSocket = welcomeData.accept(); 
+                DataOutputStream outToServer = new DataOutputStream(ControlSocket.getOutputStream());
+                DataInputStream inFromServer = new DataInputStream(new BufferedInputStream(ControlSocket.getInputStream()));
 
- 	                DataInputStream inData = new DataInputStream(new BufferedInputStream (dataSocket.getInputStream()));
-                    while(notEnd) 
-                    {
+                sentence = inFromUser.readLine();
+
+                if (sentence.equals("list:")) 
+                {
+
+                    port = port + 2;
+                    outToServer.writeBytes(port + " " + sentence + " " + '\n');
+
+                    ServerSocket welcomeData = new ServerSocket(port);
+                    Socket dataSocket = welcomeData.accept();
+
+                    DataInputStream inData = new DataInputStream(new BufferedInputStream(dataSocket.getInputStream()));
+
+                    while (notEnd) {
                         modifiedSentence = inData.readUTF();
-                        if(modifiedSentence.equals("EOF")){
+                        if (modifiedSentence.equals("EOF")) {
                             notEnd = false;
-                        }
-                        else
+                        } else 
                             System.out.print(modifiedSentence);
+                        
                     }
-	                welcomeData.close();
-	                dataSocket.close();
-                }
-                else if(sentence.startsWith("retr: "))
-                {
-                   // ....................................................
-                }
-                else if(sentence.startsWith("stor: "))
-                {
-                   // ....................................................
-                }
-                else if(sentence.startsWith("quit"))
+                    inData.close();
+                    welcomeData.close();
+                    dataSocket.close();
+                    notEnd= true;
+                } 
+                else if (sentence.startsWith("retr: "))
+                { 
+                    // ....................................................
+                } 
+                else if (sentence.startsWith("stor: "))
+                { 
+                    // ....................................................
+                } 
+                else if (sentence.startsWith("quit")) 
                 {
                     port = port + 2;
-                    outToServer.writeBytes (port + " " + sentence + " " + '\n');
+                    outToServer.writeBytes(port + " " + sentence + " " + '\n');
                     isOpen = false;
                     System.out.println("Closing Connection, Goodbye!");
                     ControlSocket.close();
-                }
-                else
-                {
-
-                }
+                } else {}
             }
-        }  
+        }
     }
     public static String parse_string(String myString) {
         String str = myString.split(" ")[0];
-        str = str.replace("\\","");
+        str = str.replace("\\", "");
         String[] arr = str.split("u");
         String text = "";
-        for(int i = 1; i < arr.length; i++){
-        int hexVal = Integer.parseInt(arr[i], 16);
-        text += (char)hexVal;
+        for (int i = 1; i < arr.length; i ++) {
+            int hexVal = Integer.parseInt(arr[i], 16);
+            text += (char)hexVal;
         }
         return text;
     }
--- ftpserver.java
+++ ftpserver.java
@@ -11,12 +11,14 @@ class ftpserver {
     String clientCommand;
     byte[] data;
     int port;
+    boolean connectionIsLive = true;
 
     ServerSocket welcomeSocket = new ServerSocket(12000);
     String frstln;
 
-    while (true) {
-      Socket connectionSocket = welcomeSocket.accept();
+    Socket connectionSocket = welcomeSocket.accept();
+
+    while (connectionIsLive) {
 
       DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
       BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
@@ -36,19 +38,22 @@ class ftpserver {
         final File folder = new File("file_folder/");
 
 
-        ArrayList<String> fileNames = new ArrayList<String>(); 
+        // ArrayList<String> fileNames = new ArrayList<String>(); 
         File[] temp = folder.listFiles();
         if(temp == null){
+          dataOutToClient.writeUTF("Folder does not exist.\n");
         System.out.println("FIXME");
         }
         for (int i=0; i < temp.length; ++i) {
                 dataOutToClient.writeUTF (temp[i].getName() + "" + '\n');  
         }
         dataOutToClient.writeUTF("EOF");
-
+        dataOutToClient.close();
         dataSocket.close();
         System.out.println("Data Socket closed");
       }
+
+      
       if (clientCommand.equals("retr:")) {
         // ..............................
         // ..............................
@@ -60,6 +65,7 @@ class ftpserver {
       if (clientCommand.equals("quit")) {
         System.out.println(connectionSocket.getInetAddress() + " disconnected");
         connectionSocket.close();
+        connectionIsLive = false;
       }
     }
   }
