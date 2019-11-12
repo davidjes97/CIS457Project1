811a0830129653c4922b85e1455207cb0168f9ff
dejongry <dejongr@mail.gvsu.edu>
2019-11-11 21:30:46 -0500

    Some Small change to GUI

--- CentralServer/centralServer.java
+++ CentralServer/centralServer.java
@@ -1,6 +1,6 @@
-import java.io. *;
-import java.net. *;
-import java.util. *;
+import java.io.*;
+import java.net.*;
+import java.util.*;
 
 import javax.xml.parsers.*;
 
@@ -15,12 +15,13 @@ class centralServer {
 
         } catch (IOException ioEx) {
             System.out.println("\nUnable to set up port!");
+            System.out.println(ioEx);
             System.exit(1);
         }
 
         while (true) {
             Socket connectionSocket = welcomeSocket.accept();
-
+            
             System.out.println("\nNew client accepted.\n");
             ClientServerHandler handler = new ClientServerHandler(connectionSocket);
 
